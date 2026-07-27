62f128ad591805552b125abfda07a067a8897a0b
Filius Orientis <filiusorientis@gmail.com>
2026-07-27 04:05:34 +0100

    chore: raise commitlint header limit to 100 chars

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,7 +2,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'header-max-length': [2, 'always', 72],
+    'header-max-length': [2, 'always', 100],
     'scope-case': [2, 'always', 'kebab-case'],
   },
 }
