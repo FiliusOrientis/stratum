f11da826f78889b97f869bbdf999806fca8cbeb2
Filius Orientis <filiusorientis@gmail.com>
2026-07-27 04:00:12 +0100

    chore: add post-merge audit, fix commitlint 72-char, CI PR-only

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,7 +2,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'header-max-length': [2, 'always', 50],
+    'header-max-length': [2, 'always', 72],
     'scope-case': [2, 'always', 'kebab-case'],
   },
 }
