c3afc8fed2d5a2feaf305496f610f4131eb635ca
Filius Orientis <filiusorientis@gmail.com>
2026-08-01 02:19:03 +0100

    feat(web): add stratum wordmark and error boundary

---
+++ apps/web/src/components/stratum-wordmark.tsx
@@ -0,0 +1,12 @@
+interface StratumWordmarkProps {
+  className?: string
+}
+
+export function StratumWordmark({ className }: StratumWordmarkProps) {
+  return (
+    <span className={className}>
+      <img src="/icons/wordmark-light.svg" alt="Stratum" className="h-12 w-auto dark:hidden" />
+      <img src="/icons/wordmark-dark.svg" alt="Stratum" className="hidden h-12 w-auto dark:block" />
+    </span>
+  )
+}
